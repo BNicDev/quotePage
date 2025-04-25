aaddeadccf049f63cd623462a59a964ef0152d32
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2025-04-24 21:07:09 -0300

    finish

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-function fetchQuote(){
-    const url = 'https://dolarapi.com/v1/dolares'
-    fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('¡Error al obtener datos!');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data); // Acá ves toda la respuesta
-    })
-    .catch(error => {
-        console.error('Hubo un problema con la petición:', error);
-    });
+function render(data) {
+    renderQuotes(data);
+    console.log(data); // Para inspeccionar los datos si querés
 }
-fetchQuote()
+
+function renderQuotes(info) {
+    const container = document.getElementById('quotes-container');
+
+    const cardsHTML = info.map(quote => `
+        <div class="cards">
+            <div class="quote-name">${quote.nombre || quote.casa}</div>
+            <div class="compra-quote">Compra: $${quote.compra}</div>
+            <div class="venta-quote">Venta: $${quote.venta}</div>
+        </div>
+    `).join('');
+
+    container.innerHTML = cardsHTML;
+}
+
+function fetchQuote() {
+    const url = 'https://dolarapi.com/v1/dolares';
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('¡Error al obtener datos!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            render(data);
+        })
+        .catch(error => {
+            console.error('Hubo un problema con la petición:', error);
+        });
+}
+
+fetchQuote();
